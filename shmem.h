27a9692b0ef892ac628f5c60cbc62fe5696bb707
Anand J <anand.indukala@gmail.com>
2017-12-07 22:56:35 -0800

    Some rearragments

--- shmem.h
+++ shmem.h
@@ -1,2 +1,11 @@
-extern int NDESC;
-extern struct shmem shmem;
+#define NDESC (PGSIZE - sizeof(struct spinlock))/sizeof(struct desc)
+
+struct desc {
+  int pid;
+  void* reference;
+};
+
+struct shmem {
+  struct spinlock lock;
+  struct desc desc[NDESC];
+};
